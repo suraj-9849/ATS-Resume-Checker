f8720904686867882db2dbe1c230983e3e6f6f61
GUNUPURU SURAJ <146669517+suraj-9849@users.noreply.github.com>
2025-04-28 21:40:25 +0530

    Changed Bg-Color

--- create_word_count.py
+++ create_word_count.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-nltk.download('punkt_tab') 
+
+nltk.download("punkt_tab")
+
+
 def create_word_cloud(text):
     """Create a word cloud from text"""
-    wordcloud = WordCloud(
-        width=800, height=400, background_color="white", max_words=100
-    ).generate(text)
+    wordcloud = WordCloud(width=800, height=400, max_words=100).generate(text)
 
     fig, ax = plt.subplots(figsize=(10, 5))
     ax.imshow(wordcloud, interpolation="bilinear")

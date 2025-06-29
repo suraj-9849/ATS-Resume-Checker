24b7d5ef9ce79ccd4b398c46667c0e7cb386102b
GUNUPURU SURAJ <146669517+suraj-9849@users.noreply.github.com>
2025-06-30 01:44:53 +0530

    Changed the gemini model

--- get_gemini_output.py
+++ get_gemini_output.py
@@ -4,6 +4,6 @@ import os
 def get_gemini_output(pdf_text, prompt):
     load_dotenv()
     genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-2.0-flash")
     response = model.generate_content([pdf_text, prompt])
     return response.text
